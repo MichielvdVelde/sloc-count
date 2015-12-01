3b459a12a16d47d126f814bdc9d943482d71c502
Michiel van der Velde <michiel@michielvdvelde.nl>
2015-12-01 13:14:03 +0100

    Version 0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # sloc-count
-Count the number of Single Lines of Code (SLOC)
+
+This module is a very, **very** simple way to count the number of source lines as well as single line and block comments.
+
+This module could probably use some sophistication.
+
+# Install
+
+```
+npm install sloc-count
+```
+
+# Usage
+
+```js
+var slocCounter = require('sloc-count');
+
+// Read a file from disk
+var contents = require('fs').readFileSync('some-file.js', 'utf8');
+
+// Now put them through the counter
+slocCounter(contents, function(err, stats) {
+	/*
+	Stats contains something similar to this:
+	{
+		total: 61,
+		source: 49,
+		singleLineComments: 1,
+		blockComments: 1,
+		empty: 8
+	}
+	*/
+});
+```
+# Changelog
+
+* 0.0.1 - 1 December 2015
+  * Initial publish
+
+## License
+
+Copyright 2015 Michiel van der Velde.
+
+This software is licensed under [the MIT License](LICENSE).
---
+++ sloc-count.js
@@ -0,0 +1,64 @@
+
+/**
+ * Takes the contents of a file and generates a statistics object with
+ * information about the lines.
+ *
+ * The 'options' argument is reserved for future use
+*/
+exports = module.exports = function(contents, options, callback) {
+	if(!callback && typeof options === 'function') {
+		callback = options;
+		options = {};
+	}
+
+	var statistics = {
+		'total': 0,
+		'source': 0,
+		'singleLineComments': 0,
+		'blockComments': 0,
+		'empty': 0
+	};
+
+	var lines = contents.split('\r\n');
+	var line, blockCommentOpen;
+	for(var i = 0; i < lines.length; i++) {
+		statistics.total++;
+
+		line = lines[i].trim();
+		switch(lineType(line)) {
+			case 'empty':
+				statistics.empty++;
+			break;
+			case 'singleLineComment':
+				statistics.singleLineComments++;
+			break;
+			case 'blockCommentOpen':
+				blockCommentOpen = true;
+			break;
+			case 'blockCommentClose':
+				if(blockCommentOpen) {
+					statistics.blockComments++;
+					blockCommentOpen = false;
+				}
+			break;
+			case 'source':
+				if(!blockCommentOpen) {
+					statistics.source++;
+				}
+			break;
+		}
+	}
+	return callback(null, statistics);
+
+};
+
+/**
+ * Method that determines when kind of line we have
+*/
+var lineType = function(line) {
+	if(line.length === 0) return 'empty';
+	if(line.substr(0, 2) == '//') return 'singleLineComment';
+	if(line.substr(0, 2) == '/*') return 'blockCommentOpen';
+	if(line.substr(line.length - 2) == '*/') return 'blockCommentClose';
+	return 'source';
+};
